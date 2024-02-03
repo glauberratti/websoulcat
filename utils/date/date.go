18850b749a100c63ec3ea6372e510dc6e8104991
glauberratti <glauber.ratti.l@gmail.com>
2024-02-03 10:15:48 -0300

    Refactoring date format

--- utils/date/date.go
+++ utils/date/date.go
@@ -1,11 +1,11 @@
 package date
 
 import (
-	"fmt"
 	"time"
 )
 
 func NowDateAndTimeBR() string {
 	n := time.Now()
-	return fmt.Sprintf("%02d/%02d/%02d %02d:%02d:%02d", n.Day(), n.Month(), n.Year(), n.Hour(), n.Minute(), n.Second())
+	// return fmt.Sprintf("%02d/%02d/%02d %02d:%02d:%02d", n.Day(), n.Month(), n.Year(), n.Hour(), n.Minute(), n.Second())
+	return n.Format("02/01/2006 03:04:05")
 }
